336e04c21630b9143936e44d906106bbd7fbda12
Shivam Mamgain <shivammamgain47@gmail.com>
2017-01-03 00:50:26 +0530

    Add Verify and Refresh Token methods

--- src/api/authApi.js
+++ src/api/authApi.js
@@ -1,7 +1,5 @@
 import { GET_TOKEN_URL, REFRESH_TOKEN_URL, VERIFY_TOKEN_URL, displayNetworkError } from './apiHelpers';
 
-// TODO: Implement Token refresh and verification
-
 class AuthApi {
   static getToken(creds) {
     let payload = {
@@ -17,6 +15,36 @@ class AuthApi {
     return fetch(GET_TOKEN_URL, config).then(response =>
       response.json().then(user =>
         ({ user, response })
+      )
+    ).catch(displayNetworkError);
+  }
+
+  static verfiyToken() {
+    const token = localStorage.getItem('token');
+    const payload = { token };
+    let config = {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(payload)
+    };
+
+    return fetch(VERIFY_TOKEN_URL, config).then(response =>
+      response.ok
+    ).catch(displayNetworkError);
+  }
+
+  static refreshToken() {
+    const token = localStorage.getItem('token');
+    const payload = { token };
+    let config = {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(payload)
+    };
+
+    return fetch(REFRESH_TOKEN_URL, config).then(response =>
+      response.json().then(token =>
+        ({ token, response })
       )
     ).catch(displayNetworkError);
   }
