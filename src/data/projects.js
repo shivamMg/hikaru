b2a500ae891f6106bafed71da8323f24c0d9fbe6
Kanishk Singh <singh.kanishk07@gmail.com>
2019-07-31 21:50:07 +0530

    Add Diomedes

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,28 +1,15 @@
 export default [
   {
     "id": 1,
-    "name": "Ayy",
-    "description": "habla habla habla",
-    "sourceLink": "https://github.com",
-    "websiteLink": "https://arionmiles.me/",
-    "author": "Kanishk",
-    "authorLink": "",
-    "creator": "bulla",
-    "tags": ['python', 'web', 'html'],
-    "created_at": "2019-07-07T17:49:14.335484Z",
-    "photo": null
-  },
-  {
-    "id": 2,
-    "name": "Lmao",
-    "description": "habla habla habla",
-    "sourceLink": "https://github.com",
-    "websiteLink": "https://arionmiles.me/",
-    "author": "Bulla",
-    "authorLink": "",
-    "creator": "bulla",
-    "tags": ['javascript', 'html'],
-    "created_at": "2019-07-07T17:49:14.325484Z",
-    "photo": null
+    "name": "Diomedes",
+    "description": "Email alerts for your favorite movies about to hit theaters",
+    "sourceLink": "https://github.com/ArionMiles/diomedes",
+    "websiteLink": "https://diomedes.in",
+    "author": "Kanishk Singh",
+    "authorLink": "https://arionmiles.me/",
+    "creator": "ArionMiles",
+    "tags": ['python', 'django'],
+    "created_at": "2019-07-31T21:47:03.404430Z",
+    "photo": 'diomedes.png'
   }
 ];
