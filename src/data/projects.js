af3f975d5bccde611df989b68e6390aef1aaa30f
Kanishk Singh <singh.kanishk07@gmail.com>
2019-08-01 11:58:38 +0530

    Added original projects
    Yay! ^_^

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,6 +1,538 @@
 export default [
   {
     "id": 1,
+    "name": "Quacker News",
+    "description": "A basic Hacker News clone",
+    "sourceLink": "https://www.github.com/dumptyd/quacker-news",
+    "websiteLink": "http://quackernews-fireduck.rhcloud.com/",
+    "author": "dumptyd",
+    "authorLink": "https://www.github.com/dumptyd",
+    "creator": "shivamMg",
+    "tags": [
+      "js",
+      "html",
+      "css",
+      "nodejs"
+    ],
+    "photo": "project.png"
+  },
+  {
+    "id": 2,
+    "name": "Della",
+    "description": "Della is a Django app for managing Secret Santa/Gift Exchange.",
+    "sourceLink": "https://github.com/avinassh/della",
+    "websiteLink": "http://avi.im/della/",
+    "author": "v",
+    "authorLink": "http://avi.im",
+    "creator": "v",
+    "tags": [
+      "django",
+      "jquery",
+      "python"
+    ],
+    "photo": "project_1kdNbfh.png"
+  },
+  {
+    "id": 3,
+    "name": "Facemask",
+    "description": "A webapp to keep track of how many animals dumptyd has seen each day.",
+    "sourceLink": "https://github.com/dumptyd/facemask/",
+    "websiteLink": "https://dumptyd.github.io/facemask/",
+    "author": "dumptyd",
+    "authorLink": "https://github.com/dumptyd/",
+    "creator": "dummy",
+    "tags": [
+      "tracker",
+      "graph",
+      "data"
+    ],
+    "photo": null
+  },
+  {
+    "id": 4,
+    "name": "spotify-dl",
+    "description": "Downloads songs from any Spotify playlist or from your 'My Music' collection.",
+    "sourceLink": "https://github.com/SathyaBhat/spotify-dl",
+    "websiteLink": "http://sathyasays.com/2015/12/29/spotify-dl-download-your-spotify-my-music-songs-as-mp3/",
+    "author": "Sathya Bhat",
+    "authorLink": "https://twitter.com/sathyabhat",
+    "creator": "shivamMg",
+    "tags": [
+      "python"
+    ],
+    "photo": "project_eLNwE9B.png"
+  },
+  {
+    "id": 5,
+    "name": "Net-Neutrality-Badge",
+    "description": "Add SaveTheInternet Badge to your Facebook DP to show support for Net Neutrality",
+    "sourceLink": "https://github.com/mr-karan/NetNeutralityBadge",
+    "websiteLink": "https://net-neutrality-badge.herokuapp.com/",
+    "author": "Karan Sharma",
+    "authorLink": "https://github.com/mr-karan/",
+    "creator": "shivamMg",
+    "tags": [
+      "flask",
+      "graph-api",
+      "python"
+    ],
+    "photo": "project_tf7FauW.png"
+  },
+  {
+    "id": 6,
+    "name": "Back2Work",
+    "description": "Chrome extension to remind you to get Back to Work on certain webpages",
+    "sourceLink": "https://github.com/rhnvrm/back2work",
+    "websiteLink": "https://chrome.google.com/webstore/detail/back-to-work/njaifcgiknihkjofpbjhfpmhjicblgoo",
+    "author": "Rohan Verma",
+    "authorLink": "https://github.com/rhnvrm/",
+    "creator": "shivamMg",
+    "tags": [
+      "js",
+      "chrome-ext"
+    ],
+    "photo": "Untitled.png"
+  },
+  {
+    "id": 7,
+    "name": "hackercouch",
+    "description": "Hospitality for Hackers. To pledge your couch, file a Pull Request.",
+    "sourceLink": "https://github.com/hackercouch/hackercouch",
+    "websiteLink": "https://hackercouch.com/",
+    "author": "Nemo",
+    "authorLink": "https://github.com/captn3m0/",
+    "creator": "shivamMg",
+    "tags": [
+      "ruby",
+      "jekyll"
+    ],
+    "photo": "project_zvVPMBZ.png"
+  },
+  {
+    "id": 8,
+    "name": "favouritetokindle",
+    "description": "An app that sends links from tweets you favourite to your Kindle",
+    "sourceLink": "https://github.com/hariprasadkulkarni/favouritetokindle",
+    "websiteLink": "",
+    "author": "Hariprasad Kulkarni",
+    "authorLink": "https://github.com/hariprasadkulkarni/",
+    "creator": "shivamMg",
+    "tags": [
+      "python"
+    ],
+    "photo": "project_jWRardZ.png"
+  },
+  {
+    "id": 9,
+    "name": "scramblejs",
+    "description": "Text animation library cum jQuery plugin",
+    "sourceLink": "https://github.com/turtleDev/scramblejs",
+    "websiteLink": "https://www.npmjs.com/package/scramblejs",
+    "author": "Saravjeet 'Aman' Singh",
+    "authorLink": "https://github.com/turtleDev",
+    "creator": "shivamMg",
+    "tags": [
+      "js",
+      "jquery"
+    ],
+    "photo": "project_qNuehsQ.png"
+  },
+  {
+    "id": 10,
+    "name": "themnewsbot",
+    "description": "News Bot for telegram to post top submissions from a subreddit",
+    "sourceLink": "https://github.com/SathyaBhat/themnewsbot",
+    "websiteLink": "https://telegram.me/themnewsbot",
+    "author": "Sathya Bhat",
+    "authorLink": "https://twitter.com/sathyabhat",
+    "creator": "shivamMg",
+    "tags": [
+      "python",
+      "bot"
+    ],
+    "photo": "project_AxX9HJR.png"
+  },
+  {
+    "id": 11,
+    "name": "allie",
+    "description": "Welcome Bot for Slack teams",
+    "sourceLink": "https://github.com/devupin/allie",
+    "websiteLink": "",
+    "author": "v",
+    "authorLink": "http://avi.im/",
+    "creator": "shivamMg",
+    "tags": [
+      "python",
+      "bot"
+    ],
+    "photo": "project_GpVHcim.png"
+  },
+  {
+    "id": 12,
+    "name": "komi",
+    "description": "A simple command saver",
+    "sourceLink": "https://github.com/shivamMg/komi",
+    "websiteLink": "",
+    "author": "shivamMg",
+    "authorLink": "https://github.com/shivamMg",
+    "creator": "shivamMg",
+    "tags": [
+      "go",
+      "cli"
+    ],
+    "photo": "project_ZqTNAcv.png"
+  },
+  {
+    "id": 13,
+    "name": "derpflowy",
+    "description": "Basic Workflowy clone",
+    "sourceLink": "https://github.com/dumptyd/derpflowy",
+    "websiteLink": "https://dumptyd.github.io/derpflowy/",
+    "author": "dumptyd",
+    "authorLink": "https://github.com/dumptyd",
+    "creator": "shivamMg",
+    "tags": [
+      "js",
+      "angularjs"
+    ],
+    "photo": "project_7F3kyOb.png"
+  },
+  {
+    "id": 14,
+    "name": "Command Help",
+    "description": "Extract help text from builtin commands and man pages",
+    "sourceLink": "https://github.com/learnbyexample/command_help",
+    "websiteLink": "",
+    "author": "asicsp",
+    "authorLink": "https://github.com/learnbyexample",
+    "creator": "shivamMg",
+    "tags": [
+      "shell",
+      "cli"
+    ],
+    "photo": "project_Y62KCFJ.png"
+  },
+  {
+    "id": 15,
+    "name": "save-the-hippo",
+    "description": "Save the hippo game",
+    "sourceLink": "https://github.com/dumptyd/save-the-hippo",
+    "websiteLink": "https://dumptyd.github.io/save-the-hippo/",
+    "author": "dumptyd",
+    "authorLink": "https://github.com/dumptyd/",
+    "creator": "shivamMg",
+    "tags": [
+      "js",
+      "html",
+      "css"
+    ],
+    "photo": "Untitled_xaUndug.png"
+  },
+  {
+    "id": 16,
+    "name": "hikaru",
+    "description": "Projects Showcase Gallery for DevUp peeps",
+    "sourceLink": "https://github.com/shivammg/hikaru",
+    "websiteLink": "https://gallery.devup.in/",
+    "author": "shivamMg",
+    "authorLink": "https://github.com/shivamMg",
+    "creator": "shivamMg",
+    "tags": [
+      "js",
+      "reactjs"
+    ],
+    "photo": "Untitled_ZvXI2Z6.png"
+  },
+  {
+    "id": 17,
+    "name": "atom-dark-plus-ui",
+    "description": "A dark UI theme for Atom forked from the Atom Dark UI theme",
+    "sourceLink": "https://github.com/ramkumar-kr/atom-dark-plus-ui",
+    "websiteLink": "https://atom.io/themes/atom-dark-plus-ui",
+    "author": "ramkumar-kr",
+    "authorLink": "https://github.com/ramkumar-kr",
+    "creator": "shivamMg",
+    "tags": [
+      "editor",
+      "theme"
+    ],
+    "photo": "Untitled_nmoN28A.png"
+  },
+  {
+    "id": 18,
+    "name": "Search for Reddit",
+    "description": "Search For Reddit shows up on the share menu in your phone allowing you to share URLs and text to the app. The app will then search reddit to find out if the URL/text or anything similar has been submitted to any subreddit previously.",
+    "sourceLink": "https://github.com/chaitanyanettem/searchforreddit",
+    "websiteLink": "https://play.google.com/store/apps/details?id=chaitanya.im.searchforreddit",
+    "author": "Chaitanya Nettem",
+    "authorLink": "http://chaitanya.im",
+    "creator": "chaitanyanettem",
+    "tags": [
+      "java",
+      "android"
+    ],
+    "photo": "project_Pg2HwFV.png"
+  },
+  {
+    "id": 19,
+    "name": "Fast News",
+    "description": "A Lightweight application which aggregates popular news articles from the web and presents concise summaries in a minimalist user interface.",
+    "sourceLink": "https://github.com/SalmaanP/news_website",
+    "websiteLink": "http://www.fastnews.me",
+    "author": "Salmaan Pehlari",
+    "authorLink": "http://www.salmaan.me",
+    "creator": "SalmaanP",
+    "tags": [
+      "python",
+      "nodejs",
+      "redis",
+      "angularjs",
+      "mongodb"
+    ],
+    "photo": "project_LP4wQcK.png"
+  },
+  {
+    "id": 20,
+    "name": "Tumibot - An autoreply bot",
+    "description": "A simple autoreply bot for Telegram which replies to random messages at random times with a random weightage",
+    "sourceLink": "https://github.com/SathyaBhat/tumibot",
+    "websiteLink": "https://github.com/SathyaBhat/tumibot",
+    "author": "Sathya Bhat",
+    "authorLink": "https://github.com/SathyaBhat",
+    "creator": "sathyabhat",
+    "tags": [
+      "ruby",
+      "telegram",
+      "bot"
+    ],
+    "photo": "project_hJeDQg2.png"
+  },
+  {
+    "id": 21,
+    "name": "Refeed",
+    "description": "Refeed is a small webapp for groups to come together and curate a stream of content for others to consume. Re-feed provides a web interface to add links then generates an RSS out of that which can be consumed easily by any RSS reader out there.",
+    "sourceLink": "https://github.com/anirudhvarma12/refeed",
+    "websiteLink": "http://linksbydevs.in/",
+    "author": "avarma12",
+    "authorLink": "https://github.com/anirudhvarma12",
+    "creator": "avarma12",
+    "tags": [
+      "html",
+      "css",
+      "python"
+    ],
+    "photo": "project_wDa4WtW.png"
+  },
+  {
+    "id": 22,
+    "name": "snooplay",
+    "description": "A webapp which shows YouTube Videos from your favourite subreddit in a playlist like format",
+    "sourceLink": "https://github.com/anirudhvarma12/snooplay",
+    "websiteLink": "https://anirudhvarma12.github.io/snooplay/src/",
+    "author": "avarma12",
+    "authorLink": "https://github.com/anirudhvarma12",
+    "creator": "avarma12",
+    "tags": [
+      "js",
+      "reactjs"
+    ],
+    "photo": "Untitled_4DkkeJ2.png"
+  },
+  {
+    "id": 23,
+    "name": "webkin",
+    "description": "CLI tool to send webpages to kindle",
+    "sourceLink": "https://github.com/mr-karan/webkin",
+    "websiteLink": "https://github.com/mr-karan/webkin",
+    "author": "Karan Sharma",
+    "authorLink": "https://github.com/mr-karan/",
+    "creator": "mrkaran",
+    "tags": [
+      "python",
+      "cli"
+    ],
+    "photo": "project_Vu09SzE.png"
+  },
+  {
+    "id": 24,
+    "name": "JobExcel",
+    "description": "Useful for making a list of companies where you wish to apply in the future for jobs.",
+    "sourceLink": "https://github.com/karunk/JobExcel",
+    "websiteLink": "https://jobexcel.herokuapp.com/",
+    "author": "karunk",
+    "authorLink": "",
+    "creator": "karunk",
+    "tags": [
+      "nodejs",
+      "angularjs",
+      "mongodb"
+    ],
+    "photo": "project_jGZxKRV.png"
+  },
+  {
+    "id": 25,
+    "name": "History Bleed",
+    "description": "Chrome Extension which checks all your browser history to see if any of the sites you visited affected by Cloud Bleed.",
+    "sourceLink": "https://github.com/avinassh/history-bleed",
+    "websiteLink": "http://avi.im/history-bleed/",
+    "author": "v",
+    "authorLink": "http://avi.im",
+    "creator": "v",
+    "tags": [
+      "js",
+      "chrome-ext"
+    ],
+    "photo": "project_6hiL8hj.png"
+  },
+  {
+    "id": 26,
+    "name": "smurf",
+    "description": "Simple markdown surfer.\n\nPreview any folder w/ Markdown files (and other files) using a website.",
+    "sourceLink": "https://github.com/oxalorg/smurf",
+    "websiteLink": "https://github.com/oxalorg/smurf",
+    "author": "oxalorg",
+    "authorLink": "https://oxal.org/",
+    "creator": "oxalorg",
+    "tags": [
+      "markdown",
+      "server",
+      "server",
+      "web",
+      "localhost"
+    ],
+    "photo": "project_jUXZvGj.png"
+  },
+  {
+    "id": 27,
+    "name": "Refract",
+    "description": "Refract is a Jukebox for public Slack teams",
+    "sourceLink": "http://github.com/turtledev/refract",
+    "websiteLink": "http://refract.turtledev.in",
+    "author": "Saravjeet 'Aman' Singh",
+    "authorLink": "http://github.com/turtledev",
+    "creator": "turtledev",
+    "tags": [
+      "reactjs",
+      "music",
+      "jukebox",
+      "slack"
+    ],
+    "photo": "Spectacle.nSd3152.png"
+  },
+  {
+    "id": 28,
+    "name": "Github Vue",
+    "description": "Simple Vue JS app to display your latest Github Commits",
+    "sourceLink": "http://github.com/avinassh/gh-vue",
+    "websiteLink": "http://avi.im/gh-vue/",
+    "author": "v",
+    "authorLink": "http://avi.im",
+    "creator": "v",
+    "tags": [
+      "js",
+      "vuejs"
+    ],
+    "photo": "project_jWJYfZY.png"
+  },
+  {
+    "id": 29,
+    "name": "Hot or Cold GPS Treasure Hunt",
+    "description": "Hot Or Cold GPS Treasure Hunt is a GPS game which uses \"Hot\" and \"Cold\" instead of directions to guide the people to the target.",
+    "sourceLink": "https://play.google.com/store/apps/details?id=com.shawnrebello.hotorcold251239",
+    "websiteLink": "https://play.google.com/store/apps/details?id=com.shawnrebello.hotorcold251239",
+    "author": "Shawn Rebello",
+    "authorLink": "http://shawnrebello.com",
+    "creator": "shawnrebello",
+    "tags": [
+      "angularjs",
+      "android",
+      "ionic"
+    ],
+    "photo": "project_7J9r0iA.png"
+  },
+  {
+    "id": 31,
+    "name": "email-actions",
+    "description": "email-actions is a tiny SMTP server with a rules based engine to trigger any actions (notifications/commands etc) based on the emails sent to this server",
+    "sourceLink": "https://github.com/shantanugoel/email-actions",
+    "websiteLink": "https://shantanugoel.com/2017/04/03/email-actions-trigger-tasks-from-email-smtp-server/",
+    "author": "Shantanu Goel",
+    "authorLink": "https://shantanugoel.com/",
+    "creator": "shivamMg",
+    "tags": [
+      "python",
+      "server",
+      "smtp"
+    ],
+    "photo": "project_58PV6io.png"
+  },
+  {
+    "id": 32,
+    "name": "Command-line-text-processing",
+    "description": "From finding text to search and replace, from sorting to beautifying text and more.",
+    "sourceLink": "https://github.com/learnbyexample/Command-line-text-processing",
+    "websiteLink": "https://learnbyexample.gitbooks.io/command-line-text-processing/content/",
+    "author": "asicsp",
+    "authorLink": "https://github.com/learnbyexample",
+    "creator": "shivamMg",
+    "tags": [
+      "shell",
+      "cli",
+      "book"
+    ],
+    "photo": "project_OnLp7eF.png"
+  },
+  {
+    "id": 33,
+    "name": "malvo",
+    "description": "Programming contest platform",
+    "sourceLink": "https://github.com/shivammg/malvo",
+    "websiteLink": "http://malvo.shivammamgain.com/",
+    "author": "shivamMg",
+    "authorLink": "http://shivammamgain.com/",
+    "creator": "shivamMg",
+    "tags": [
+      "django",
+      "python",
+      "semantic-ui",
+      "docker"
+    ],
+    "photo": "project_V4Y602X.png"
+  },
+  {
+    "id": 34,
+    "name": "sakura",
+    "description": "A minimal CSS Framework/Theme",
+    "sourceLink": "https://github.com/oxalorg/sakura",
+    "websiteLink": "https://oxal.org/projects/sakura/demo",
+    "author": "oxalorg",
+    "authorLink": "https://oxal.org",
+    "creator": "shivamMg",
+    "tags": [
+      "css",
+      "framework"
+    ],
+    "photo": "Screenshot_20170503_124140.png"
+  },
+  {
+    "id": 35,
+    "name": "CodeChef Offline",
+    "description": "An offline problem browser for Codechef problems. You can clone the repo and browse all problems offline",
+    "sourceLink": "https://github.com/captn3m0/codechef",
+    "websiteLink": "https://captnemo.in/codechef",
+    "author": "Nemo",
+    "authorLink": "https://captnemo.in",
+    "creator": "nemo",
+    "tags": [
+      "markdown",
+      "codechef",
+      "scraper",
+      "php"
+    ],
+    "photo": "project_sDE2c97.png"
+  },
+  {
+    "id": 36,
     "name": "Diomedes",
     "description": "Email alerts for your favorite movies about to hit theaters",
     "sourceLink": "https://github.com/ArionMiles/diomedes",
@@ -8,7 +540,10 @@ export default [
     "author": "Kanishk Singh",
     "authorLink": "https://arionmiles.me/",
     "creator": "ArionMiles",
-    "tags": ['python', 'django'],
-    "photo": 'diomedes.png'
+    "tags": [
+      "python",
+      "django"
+    ],
+    "photo": "diomedes.png"
   }
 ];
